661d1f9d6991cbe310e2890440ade58da621f45a
kyrosortopedico-create <kyrosortopedico@gmail.com>
2026-05-02 15:24:04 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,21 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+const CACHE_NAME = "afp-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
